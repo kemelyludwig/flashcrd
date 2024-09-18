f55398e9dc53d3ac8947f4f26ea68bfee423caed
kemelyludwig <130472722+kemelyludwig@users.noreply.github.com>
2024-09-18 19:36:43 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,29 @@
+criaCartao(
+    'História',
+    'Qual o nome do presidente do Brasil que ficou conhecido como Jango?',
+    'João Goulart.'
+)
+
+criaCartao(
+    'Física',
+    'Qual a capital da FrançDe onde é a invenção do chuveiro elétrico??',
+    'Brasil.'
+)
+
+criaCartao(
+    'Filosofia',
+    'De quem é a famosa frase “Penso, logo existo”??',
+    'Descartes.'
+)
+
+criaCartao(
+    'Química',
+    'Atualmente, quantos elementos químicos a tabela periódica possui?',
+    'Infinitas.'
+)
+
+criaCartao(
+    'Matemática',
+    'Quantas casas decimais tem o número pi?',
+    'Infinitas.'
+)
